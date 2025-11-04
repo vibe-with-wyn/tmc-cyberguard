536aa0849fd67d67817703e6691cbf39a817499d
Wyndale <lapurawendel.dev@gmail.com>
2025-11-04 12:47:44 +0800

    chore: initial Spring Boot project setup

---
+++ src/main/java/com/tmc/system/tmc_secure_system/TmcSecureSystemApplication.java
@@ -0,0 +1,13 @@
+package com.tmc.system.tmc_secure_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcSecureSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmcSecureSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmc/system/tmc_secure_system/TmcSecureSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmc.system.tmc_secure_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmcSecureSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
